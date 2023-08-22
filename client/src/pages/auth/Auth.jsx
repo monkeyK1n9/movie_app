b2fce6a67596456444731464bb426bed50f3c682
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-08-22 20:32:08 +0100

    create auth skeleton

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -2,6 +2,24 @@ import "./auth.scss";
 
 export default function Auth() {
     return (
-        <div className='auth'>Auth</div>
+        <div className='auth'>
+            <div className="top">
+                <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" className="logo" />
+
+                <button className="loginButton">Sign In</button>
+            </div>
+
+            <div className="container">
+                <h1>Unlimited movies, TV shows, and more.</h1>
+                <h2>Watch anywhere. Cancel anytime.</h2>
+                <p>
+                    Ready to watch? Enter your mail to create or restart your membership.
+                </p>
+                <div className="input">
+                    <input type="email" placeholder="email address"/>
+                    <button className="registerButton">Get Started</button>
+                </div>
+            </div>
+        </div>
     )
 }
